a4efba073593cb3293d5b9a4805b8b80a58bad39
Bülent Özen <bulentozen94@gmail.com>
2017-12-12 17:54:27 +0300

    Add project files.

---
+++ AngularNorthwind/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AngularNorthwind.MvcApplication" Language="C#" %>
---
+++ AngularNorthwind/Models/Model/ProductsModel.cs
@@ -0,0 +1,15 @@
+﻿using AngularNorthwind.Models.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AngularNorthwind.Models.Model
+{
+    public class ProductsModel
+    {
+        public List<Products> plist { get; set; }
+        public List<CategoriesSelect> clist { get; set; }
+        public List<SuppliersSelect> slist { get; set; }
+    }
+}
